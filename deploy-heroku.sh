eb796eab8982618eaafb3282645cda8391fa4e79
jansson-daniel <danielwallin@Daniels-MacBook-Pro.local>
2018-12-27 16:15:20 +0100

    yml

--- deploy-heroku.sh
+++ deploy-heroku.sh
@@ -3,7 +3,6 @@ set -u
 source ./setup-env.sh
 echo "Pushing branch ${CIRCLE_BRANCH} to app ${HEROKU_APP}"
 git remote add heroku https://git.heroku.com/${HEROKU_APP}.git
-git pull heroku ${CIRCLE_BRANCH}:master
 git push heroku ${CIRCLE_BRANCH}:master
 
 if [ ! -L /usr/local/bin/heroku ];
