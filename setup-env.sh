4211ade4f15ad08bb5eadccb1f533f5e40da5d63
jansson-daniel <danielwallin@Daniels-MacBook-Pro.local>
2018-12-27 15:21:09 +0100

    yml

--- setup-env.sh
+++ setup-env.sh
@@ -3,9 +3,13 @@ case $CIRCLE_BRANCH in
         export ENVIRONMENT="develop"
         export HEROKU_APP="hairsalons-develop"
         ;;
-    "master")
+    "develop")
         export ENVIRONMENT="staging"
         export HEROKU_APP="hairsalons-stage"
+        ;;
+    "master")
+        export ENVIRONMENT="production"
+        export HEROKU_APP="hairsalons-production"
         ;;
 esac
 export NODE_ENV="production"
